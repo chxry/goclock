ea09499fe740a5d16a9a3da3e7f51802226f1c2b
chxry <moosejuicetex@gmail.com>
2021-04-06 17:33:27 +0100

    Added colours, Removed borders on characters and cleaned up

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/chxry/goclock
 
 go 1.16
-
-require github.com/wayneashleyberry/terminal-dimensions v1.0.0
--- goclock.go
+++ goclock.go
@@ -1,42 +1,66 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
 const clearCode string = "\033[H\033[2J"
 
-var characters = map[rune][5]string{
-	'0': {"██████", "█    █", "█    █", "█    █", "██████"},
-	'1': {"  ██  ", "  ██  ", "  ██  ", "  ██  ", "  ██  "},
-	'2': {"██████", "     █", "██████", "█     ", "██████"},
-	'3': {"██████", "     █", "██████", "     █", "██████"},
-	'4': {"█    █", "█    █", "██████", "     █", "     █"},
-	'5': {"██████", "█     ", "██████", "     █", "██████"},
-	'6': {"██████", "█     ", "██████", "█    █", "██████"},
-	'7': {"██████", "     █", "     █", "     █", "     █"},
-	'8': {"██████", "█    █", "██████", "█    █", "██████"},
-	'9': {"██████", "█    █", "██████", "     █", "     █"},
-	':': {"      ", "  ██  ", "      ", "  ██  ", "      "}}
+var colorCode = map[string]string{
+	"red":     "\u001b[41m",
+	"green":   "\u001b[42m",
+	"yellow":  "\u001b[43m",
+	"blue":    "\u001b[44m",
+	"magenta": "\u001b[45m",
+	"cyan":    "\u001b[46m",
+	"white":   "\u001b[47m",
+}
+
+var full = [6]bool{true, true, true, true, true, true}
+var empty = [6]bool{false, false, false, false, false, false}
+var left = [6]bool{true, true, false, false, false, false}
+var right = [6]bool{false, false, false, false, true, true}
+var outer = [6]bool{true, true, false, false, true, true}
+var inner = [6]bool{false, false, true, true, false, false}
+
+var characters = map[rune][5][6]bool{
+	'0': {full, outer, outer, outer, full},
+	'1': {inner, inner, inner, inner, inner},
+	'2': {full, right, full, left, full},
+	'3': {full, right, full, right, full},
+	'4': {outer, outer, full, right, right},
+	'5': {full, left, full, right, full},
+	'6': {full, left, full, outer, full},
+	'7': {full, right, right, right, right},
+	'8': {full, outer, full, outer, full},
+	'9': {full, outer, full, right, right},
+	':': {empty, inner, empty, inner, empty}}
 
 func main() {
-	for {
-		//var x, _ = terminal.Width()
-		var currentTime = time.Now().Format("15:04") //hh:mm
-		var output = ""
+	var color = flag.String("color", "white", "The color to use.\nOptions: 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'")
+	flag.Parse()
 
+	for {
+		var currentTime = time.Now().Format("15:04") //hh:mm
+		//var currentTime = "0123456789:" //test font
+		var output = ""
 		for i := 0; i < 5; i++ {
 			for _, c := range currentTime {
-				output += characters[c][i]
+				for ii := 0; ii < 6; ii++ {
+					if characters[c][i][ii] {
+						output += colorCode[*color] + " \u001b[0m"
+					} else {
+						output += " "
+					}
+				}
 				output += "  "
 			}
 			output += "\n"
 		}
-
 		fmt.Print(
 			clearCode,
-			//strings.Repeat("#", int(x/2)),
 			output,
 		)
 		time.Sleep(1 * time.Second)
